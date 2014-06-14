29612fe00e556f379e6a047195a2b7b9fba8d469
Lasse Nørgaard <lassenorgaard@Lasses-MacBook-Pro-2.local>
2014-06-15 00:09:26 +0200

    Added new folders and files

--- index.php
+++ index.php
@@ -4,13 +4,27 @@
   <meta charset="utf-8">
   <title>Basic site</title>
   <meta name="author" content="lassenorgaard.dk">
-  <link rel="stylesheet" href="css/screen.css">
+  <link rel="stylesheet" href="css/screen.css" media="screen">
   <link rel="stylesheet" href="css/print.css" media="print" />
 </head>
 <body>
+<div class="page-wrapper">
+  <header>
+    <div class="inner">
+      <?php require_once('includes/header.php'); ?>
+    </div>
+  </header>
 
+  <section class="main">
+  Hello
+  </section>
 
-
+  <footer>
+    <div class="inner">
+      <?php require_once('includes/footer.php'); ?>
+    </div>
+  </footer>
+</div>
 
   <script src="js/main.js"></script>
 </body>
