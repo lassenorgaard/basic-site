d10309c9a52a2f3d335267957a7338f194d09771
Lasse <lasse@adapt.dk>
2014-06-14 23:07:14 +0200

    Added files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Basic site</title>
+  <meta name="author" content="lassenorgaard.dk">
+  <link rel="stylesheet" href="css/screen.css">
+  <link rel="stylesheet" href="css/print.css" media="print" />
+</head>
+<body>
+
+
+
+
+  <script src="js/main.js"></script>
+</body>
+</html>
